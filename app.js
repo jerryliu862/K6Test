a98da1bb137452ecd2fd2ed2f8eeed06d4520e59
jerry <jerry_cj_liu@wistron.com>
2022-09-21 11:46:02 +0800

    feat add 50 fans

--- app.js
+++ app.js
@@ -1,81 +1,69 @@
-const { v4: uuidv4 } = require('uuid');
 const web3 = require('@solana/web3.js');
 const crypto = require('crypto');
+const axios = require('axios').default;
+
 /* create wallet */
-this.createWallet = async function () {
-    let rsp = {
-      success: false,
-      publicKey: '',
-      secretKey: '',
-      msg: '',
-      chain: this.chain,
-    };
-    return new Promise((resolve, reject) => {
-      try {
-        let w = web3.Keypair.generate();
-        rsp.publicKey = w.publicKey.toBase58();
-        // encrypt
-        try {
-          rsp.secretKey = cipherEncrypt(w.secretKey.join(',')).data;
-          rsp.success = true;
-          resolve(rsp);
-        } catch (e) {
-          rsp.msg = `Failed to create new wallet: ${e.message}`;
-          resolve(rsp);
-        }
-      } catch (e) {
-        rsp.msg = `Failed to create new wallet: ${e.message}`;
-        resolve(rsp);
+// createUser();
+
+const insertMember = async(username, walletAddress) =>{
+  const url = 'https://retool-api.hotline-qa.io/graphql';
+  const data = {
+    query: `mutation insertUser($username:String!, $walletAddress:String!){
+      insertUser(username: $username, walletAddress:$walletAddress) {
+        status
+        message
       }
- 
-    });
-};
-  /* CPIHER - encrypt */
-  function cipherEncrypt(_data) {
-    const res = {
-      success: false,
-      msg: '',
-      data: '',
-    };
-
-    try {
-    // const k = self.getEnv('CIPHER_KEY');
-    // const i = self.getEnv('CIPHER_IV');
-      const k = 'NCKDJCNFHWPDJJDUCNQPDIOKDLEJSCND';
-      const i = '8DFED197BA48531E';
-      const cipher = crypto.createCipheriv('aes-256-cbc', k, i);
-      buffer = Buffer.concat([
-        cipher.update(_data),
-        cipher.final()
-      ]);
-      // set data
-      res.data = buffer.toString('base64');
-      res.success = true;
-    } catch (e) {
-      res.msg = e.message;
     }
-    //
-    return res;
-  }
-const insertList = [];  
-const createUser = async() =>{
-    
-    for(let i=0 ;i<=50; i++){
-        const tempWalletRes = await this.createWallet();
-        if (tempWalletRes?.success !== true) {
-          return 'Creating temp wallet failed';
-        }
-        const tempWalletData = {};
-        // tempWalletData.memberId = "ad94a0f2-ae04-4127-abd8-a2865181f69b";
-        // tempWalletData.orderId = uuidv4();
-        tempWalletData.publicKey = tempWalletRes.publicKey;
-        tempWalletData.secretKey = tempWalletRes.secretKey;
-        // tempWalletData.createdBy = "ad94a0f2-ae04-4127-abd8-a2865181f69b";
-        tempWalletData.createdAt = new Date();
-        insertList.push(tempWalletData);
-
-    }
-    
-    console.log(insertList);
+    `,
+    variables: `{
+    "username": "${username}",
+    "walletAddress": "${walletAddress}"
+    }`,
+  };
+  // console.log(data,'data');
+  const response = await httpPost(url, data, {
+    'RETOOL-API-KEY': 'retool_dev_api_key',
+    'Content-Type': 'application/json',
+  });
 }
-createUser();
+
+const httpPost = async(url, requestBody , headers ) => {
+  try {
+    const response = await axios.post(url, requestBody, { headers });
+    console.log(response.data.data.insertUser.message,'------success');
+    // return response?.data;
+  } catch (error) {
+    console.log('---GQL Bad Request---');
+    console.log(error.status);
+  }
+}
+
+
+// console.log(`test-${new Date().valueOf()}`  )
+// insertMember(`0test${new Date().valueOf()}`, '4nB41TiWPP5VZ7a4qZH9UhDS9MvyBunkgoTrVnhyN5pn')
+
+const addFans = async()=>{
+  let rsp = {
+    publicKey: '',
+    secretKey: '',
+  };
+  let w = web3.Keypair.generate();
+  rsp.publicKey = w.publicKey.toBase58();
+  const k = 'NCKDJCNFHWPDJJDUCNQPDIOKDLEJSCND';
+  const i = '8DFED197BA48531E';
+  const cipher = crypto.createCipheriv('aes-256-cbc', k, i);
+  buffer = Buffer.concat([
+    cipher.update(w.secretKey.join(',')),
+    cipher.final()
+  ]);
+  // set data
+  rsp.secretKey = buffer.toString('base64');
+  // console.log(rsp,'rsp.publicKey');
+  insertMember(`0test${new Date().valueOf()}`, rsp.publicKey)
+
+}
+
+// console.log(addFans());
+for(let i=0; i<=50; i++){
+  addFans()
+}
--- profile_upload_test.js
+++ profile_upload_test.js
@@ -1,13 +1,17 @@
 import http from "k6/http";
 import { FormData } from 'https://jslib.k6.io/formdata/0.0.2/index.js';
-
+export const options = {
+  stages: [
+    { duration: '15s', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 1 minutes.
+    { duration: '30s', target: 100 }, // stay at 100 users for 10 minutes
+    { duration: '15s', target: 0 }, // ramp-down to 0 users
+  ],
+};
 
 const img = open("./01.jpg", "b");
 // const img1 = open('0')
 
 const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwdWJsaWNLZXkiOiJwNGVuZ0txR3N4YzNrZFF0U2N2UXJWb1llZ2JHN3hrdHJTNFhESlRBVkxEIiwibWVtYmVySWQiOiIyOTgzYTc2MC1mYmY5LTRhODMtOTg2Yi1hNTI1MWFiZTI0ZDciLCJpYXQiOjE2NDY4MjE3MjgsImV4cCI6OTY3ODc4NTAzMH0.7irKXuLC8sCYk9tec6uBWI-MppHkzDQkdk4YCTEf3fM';
-
-
 
 
 export default function() {
@@ -16,7 +20,6 @@ export default function() {
   const headers = {
     'access-token': accessToken,
     'Content-Type': 'multipart/form-data; boundary=' + fd.boundary,
-    // 'User-Agent': "k6/0.37.0 (https://k6.io/)"
   };
     const data = {
       file: http.file(img, '01.jpg'),
@@ -27,7 +30,7 @@ export default function() {
     let res = http.post('https://media-backend.hotline-qa.io/media/uploadtos3', fd.body(), {
       headers: headers,
     });
-    console.log(res.body,'body')
+    // console.log(res.body,'body')
 }
   
 
